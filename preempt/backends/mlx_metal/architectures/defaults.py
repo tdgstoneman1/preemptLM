0187fff28bb58ccd087376b2db3addc598fc895f
David S <david@Davids-MacBook-Pro.local>
2026-08-19 15:45:08 -0400

    defaults.py: renamed DEFAULT_MOE_FACTORIES to V1_MOE_FACTORIES

--- preempt/backends/mlx_metal/architectures/defaults.py
+++ preempt/backends/mlx_metal/architectures/defaults.py
@@ -4,9 +4,6 @@ from ..types import MlxMoEFactory
 
 from .qwen3_next import Qwen3NextMoEArchitecture
 
-# TODO add version
-# TODO rename "qwen" key to "qwen3-next"
-
-DEFAULT_MOE_FACTORIES: Final[dict[str, MlxMoEFactory]] = {
+V1_MOE_FACTORIES: Final[dict[str, MlxMoEFactory]] = {
     "qwen": Qwen3NextMoEArchitecture
-}
+}  # TODO rename "qwen" key to "qwen3-next"
